bb8c01f392164a0ceac1646cd2a71f1b6e76dfef
Gerald O <gerald.onyango@gmail.com>
2018-09-21 17:17:44 -0400

    updated generate_jwt.js function so it should work

--- functions/generate_jwt.js
+++ functions/generate_jwt.js
@@ -1,19 +1,34 @@
 const jwt = require('jsonwebtoken');
 
-const generateJwt = (claims)=>{
-    jwt.sign({
-        exp: Math.floor(Date.now() / 1000) + (60 * 60),
-        data: 'foobar'
+const getExpDate = () => {
+    const exp = Math.floor(Date.now() / 1000) + (60 * 60)
+    const expReadable = new Date(exp);
+    return {exp: exp,readable: expReadable}
+} 
+
+const generateJwt = ({claims, exp})=>{
+    return jwt.sign({
+        exp: exp,
+        data: {
+            app_metadata: {
+            "roles": [
+              "user",
+              "cms"
+            ]
+          },
+          "user_metadata": claims
+        }
       }, 'superSecret');
 
 }
 
 exports.handler = function (event, context, callback) {
-
-    console.log("got a request! I'm sending back the request body now")
-    console.log(event.body)
+    // parsing the inbound request
     const reqBody = JSON.parse(event.body)
-    
+    // generating exp date
+    const expTimes = getExpDate()
+    const token = generateJwt({claims: event.body, exp: expTimes.exp })
+    const response = {jwt: token, valid_through: expTimes.expReadable}
 
     callback(null, {
         statusCode: 200,
