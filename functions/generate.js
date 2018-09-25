4ad28525b328f4bee821e5cc7dc3e65be96c9580
Gerald O <gerald.onyango@gmail.com>
2018-09-25 11:47:29 -0400

    testing

--- functions/generate.js
+++ functions/generate.js
@@ -1,43 +1,40 @@
-
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 exports.handler = function(event, context, callback) {
-    const getExpDate = () => {
-        const exp = Math.floor(Date.now() / 1000) + (60 * 60)
-        // const expReadable = new Date(exp);
-        return exp
-    } 
-    
-    const generateJwt = ({claims, exp, secret})=>{ 
-        return jwt.sign({
-            exp: exp,
-            data: {
-                app_metadata: {
-                "roles": [
-                  "user",
-                  'cms'
-                ]
-              },
-              "user_metadata": claims
-            }
-          }, secret ? secret : 'superSecret');
-    
-    }
-    // parsing the inbound request
-    const parsedBody = JSON.parse(event.body)
-    console.log(parsedBody)
-    const {claims,secret}  = parsedBody
+  const getExpDate = () => {
+    const exp = Math.floor(Date.now() / 1000) + 60 * 60;
+    // const expReadable = new Date(exp);
+    return exp;
+  };
 
-    // generating exp date
-    const expTimes = getExpDate()
-    const token = generateJwt({claims: claims, exp: expTimes, secret: secret })
-    const response = {
-        "jwt": token, 
-        "exep": expTimes,
-    }
+  const generateJwt = ({ claims, exp, secret }) =>
+    jwt.sign(
+      {
+        exp,
+        data: {
+          app_metadata: {
+            roles: ['user', 'cms'],
+          },
+          user_metadata: claims,
+        },
+      },
+      secret || 'superSecret'
+    );
+  // parsing the inbound request
+  const parsedBody = JSON.parse(event.body);
+  console.log(parsedBody);
+  const { claims, secret } = parsedBody;
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(response)
-    });
-}
+  // generating exp date
+  const expTimes = getExpDate();
+  const token = generateJwt({ claims, exp: expTimes, secret });
+  const response = {
+    jwt: token,
+    exep: expTimes,
+  };
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  });
+};
