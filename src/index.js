5b94dc1fa7ec73d9d24ed7dad5c490e87e06df8c
Gerald O <gerald.onyango@gmail.com>
2018-09-25 23:12:54 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 const jwt = require('jsonwebtoken');
 const cookie = require('cookie');
 
-const verifyUser = (jwt) => {
-  jwt.verify(token, 'superSecret', { algorithms: ['HS256'] }, (err, payload) => {
+const verifyUser = token => jwt.verify(token, 'superSecret', { algorithms: ['HS256'] }, (err, payload) => {
     if (err) {
-      return {isValid: false, payload: payload}
+      return {isValid: false, payload: err.message}
     }
     return {isValid: true, payload: payload}
   });
-}
 
-const handleRequest = async (request) => {
+const handleRequest = async request => {
   const headers = new Map(request.headers);
   const cookieHeader = headers.get('cookie');
   const cookies = cookie.parse(cookieHeader);
   console.log(cookies);
   console.log(cookies.nf_jwt);
   const verifyResponse = verifyUser(cookies.nf_jwt);
-  if (!verifyResponse.isValid){
-    const response = await fetch('/404.html');
-    return response
+  console.log(verifyResponse);
+  if (!verifyResponse.isValid) {
+    const res = await fetch('https://futuregerald.app/404.html');
+    return res;
   }
   const response = await fetch(request);
   return response;
-}
+};
 
 addEventListener('fetch', event => {
+  console.log('the request url is: ',event.request.url)
   console.log(event.request);
   event.respondWith(handleRequest(event.request));
 });
-
